cf0f50db6ead9c30e39ed2d1bf3f87295ad94d75
ᴘɪxᴇʟ Pᴜsʜᴇʀ03 <153280672+pixelPusher03@users.noreply.github.com>
2024-12-03 09:30:32 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 cd .. 
 rm -rf followers-increaser
-git clone https://github.com/security-softwares/followers-increaser/
-cd followers-increaser
+git clone https://github.com/security-softwares/Instagram-followers/
+cd Instagram-followers
 bash increaser.sh
